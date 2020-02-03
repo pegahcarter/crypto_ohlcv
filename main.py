7bdaf10d7d39346d82bce3e6471db87867a951b5
Carter Carlson <carlfarterson@gmail.com>
2020-02-03 17:13:44 -0600

    Begin adding start/end dates

--- main.py
+++ main.py
@@ -16,45 +16,62 @@ def setup():
         config = yaml.load(myfile, Loader=yaml.Loader)
 
         # Save all messages to MESSAGES
+        MESSAGES['exchange'] = config['MESSAGES']['message_1']
         MESSAGES['exchange_not_found'] = config['MESSAGES']['message_1_error_handler']
-        MESSAGES['api_public_key'] = _config['MESSAGES']['message_2']
-        MESSAGES['api_private_key'] = _config['MESSAGES']['message_3']
-        MESSAGES['candle_interval'] = _config['MESSAGES']['message_4']
+        MESSAGES['api_public_key'] = config['MESSAGES']['message_2']
+        MESSAGES['api_private_key'] = config['MESSAGES']['message_3']
+        MESSAGES['ticker'] = config['MESSAGES']['message_4']
+        MESSAGES['ticker_not_found'] = config['MESSAGES']['message_4_error_handler']
+        MESSAGES['candle_interval'] = config['MESSAGES']['message_5']
+        MESSAGES['candle_interval_not_found'] = config['MESSAGES']['message_5_error_handler']
+        MESSAGES['start_date'] = config['MESSAGES']['message_6']
+        MESSAGES['start_date_not_found'] = config['MESSAGES']['message_6_error_handler']
+        MESSAGES['end_date'] = config['MESSAGES']['message_7']
+        MESSAGES['end_date_not_found'] = config['MESSAGES']['message_7_error_handler']
 
         # Save all variables to VARIABLES
         VARIABLES['exchange'] = config['VARIABLES']['EXCHANGE']
-        if VARIABLES['exchange'] is None:
-            VARIABLES['exchange'] =
-
-
-
-
-
-
-
-
-
-
-
-
+        VARIABLES['start_date'] = config['VARIABLES']['START_DATE']
+        VARIABLES['end_date'] = config['VARIABLES']['END_DATE']
 
 
 def main():
-    pass
-    # 1. Get name of exchange to pull data from and make sure exchange exists
-    # # exchange_str = input(MESSAGES['exchange'])
-    # try:
-    #     exchange = getattr(ccxt, exchange_str.lower())
-    # except:
-    #     print(MESSAGES['exchange_not_found'] % exchange_str)
-    #     return
 
-    #  2. Add API key if needed
+    #  1. Enter exchange used
+    if VARIABLES['exchange'] is None:
+        VARIABLES['exchange'] = input(MESSAGES['exchange'])
+    try:
+        exchange = getattr(ccxt, VARIABLES['exchange'].lower())()
+    except:
+        print(MESSAGES['exchange_not_found'] % VARIABLES['exchange'])
+        return
+
+    #   2. Add API public key if needed
     # api_public_key = input(MESSAGES['api_public_key'])
-    #  3. Add API api_private_keyret if api_private_key    # API_PRIVATE_KEY = input(MESSAGES['API_PRIVATE_KEY'])
+    #  3. Add API private key if needed
+    # API_PRIVATE_KEY = input(MESSAGES['API_PRIVATE_KEY'])
 
-    #   4. Get candle_interval
-    # candle_interval = input(MESSAGES['candle_interval'])
+    #  4. Enter ticker
+    VARIABLES['ticker'] = input(MESSAGES['ticker'])
+    if VARIABLES['ticker'] not in exchange.fetch_tickers():
+        print(MESSAGES['ticker_not_found'] % VARIABLES['TICKER'])
+        return
+
+    #  5. Enter candle_interval
+    VARIABLES['candle_interval'] = input(MESSAGES['candle_interval'])
+    try:
+        exchange.fetch_ohlcv(VARIABLES['ticker'], VARIABLES['candle_interval'], limit=500)
+    except:
+        print(MESSAGES['candle_interval_not_found'] % VARIABLES['candle_interval'])
+        return
+
+    #  6. Enter start_date
+    if VARIABLES['start_date'] is None:
+        VARIABLES['start_date'] = input(MESSAGES['start_date'])
+
+    #  7. Enter end_date
+    if VARIABLES['end_date'] is None:
+        VARIABLES['end_date'] = input(MESSAGES['end_date'])
 
 
 
@@ -65,12 +82,6 @@ if __name__ == '__main__':
     setup()
     main()
 
-
-'''
-To-Do
-- [ ] Display all candle interval options
-- [ ] Input of a specific start date
-'''
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
