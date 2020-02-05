24eb40824c9b4309af7c02c63424f09a8c73886d
Carter Carlson <carlfarterson@gmail.com>
2020-02-05 11:54:28 -0600

    Remove old code

--- main.py
+++ main.py
@@ -150,29 +150,3 @@ def main():
 if __name__ == '__main__':
     setup()
     main()
-
-
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-# Old code used as reference to create this file
-#
-# ticker = 'BTC/USDT'
-#
-# start_date = datetime(year=2018, month=2, day=10, hour=22)
-# df = []
-#
-# while start_date < datetime(year=2020, month=1, day=1, hour=1):
-#     data = binance.fetch_ohlcv(ticker, '1h', limit=500, since=int(time.mktime(start_date.timetuple())*1000))
-#     df.extend(data)
-#
-#     # fetching last date to
-#     data = np.array(data)
-#     last_date = datetime.fromtimestamp(data[-1, 0]/1000)
-#     start_date = last_date + timedelta(hours=1)
-#
-#     time.sleep(.1)
-#
-# df = pd.DataFrame(df, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
-# df['date'] = df['date'].apply(lambda x: datetime.fromtimestamp(x/1000))
-#
-# df.to_csv(f'binance/{ticker.replace("/", "-")}.csv', index=False)
